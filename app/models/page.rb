e1fb11e3fa4c2bfc95a2f3c6ff3a40f29aaef149
jason gagne <jgagne33@gmail.com>
2010-09-14 15:02:25 -0700

    removing titlize method

--- app/models/page.rb
+++ app/models/page.rb
@@ -12,7 +12,6 @@ class Page < ActiveRecord::Base
   default_scope :order => "parent_id, position"
   named_scope :visible, :conditions => "status = 'visible'"
   named_scope :hidden, :conditions => "status = 'hidden'"
-  before_save :titleize_title
   
   def to_param
     self.permalink
@@ -24,10 +23,6 @@ class Page < ActiveRecord::Base
   
   def description
     self.meta_description
-  end
-  
-  def titleize_title
-    self.title = self.title.titleize
   end
   
   def hidden?
