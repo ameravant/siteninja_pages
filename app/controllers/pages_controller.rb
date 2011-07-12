c307da29618391cb44066340fb5dce0fbdd12d82
Ameravant <contact@ameravant.com>
2011-07-12 16:14:34 -0700

    fixes for main column

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,8 @@ class PagesController < ApplicationController
       @menu = @page.menus.first
       @side_column_sections = ColumnSection.all(:conditions => {:column_id => @page.column_id, :visible => true})
       @side_column_sections = ColumnSection.all(:conditions => {:column_id => 1, :visible => true}) if @page.column_id.blank?
-      @page.main_column_id = Column.first(:conditions => {:title => "Default", :column_location => "main_column", :can_delete => false}) if @page.main_column_id.blank?
-      @main_column = Column.find(@page.main_column_id)
-      @main_column_sections = ColumnSection.all(:conditions => {:column_id => @page.main_column_id, :visible => true}) 
+      @main_column = (@page.main_column_id.blank? ? Column.first(:conditions => {:title => "Default", :column_location => "main_column"}) : Column.find(@page.main_column_id))
+      @main_column_sections = ColumnSection.all(:conditions => {:column_id => (@page.main_column_id.blank? ? @main_column.id : @page.main_column_id), :visible => true})
       @images = @page.images
       @footer_pages = Page.find(:all, :conditions => {:show_in_footer => true}, :order => :footer_pos )
       @features = []
