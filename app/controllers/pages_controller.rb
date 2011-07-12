dd6b520bb11a37542e155b6cbe77f6c2c8a75ce0
Ameravant <contact@ameravant.com>
2011-07-12 14:27:04 -0700

    fixed main column error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,6 @@ class PagesController < ApplicationController
       @page.main_column_id = Column.first(:conditions => {:title => "Default", :column_location => "main_column", :can_delete => false}) if @page.main_column_id.blank?
       @main_column = Column.find(@page.main_column_id)
       @main_column_sections = ColumnSection.all(:conditions => {:column_id => @page.main_column_id, :visible => true}) 
-      @main_column_sections = 
       @images = @page.images
       @footer_pages = Page.find(:all, :conditions => {:show_in_footer => true}, :order => :footer_pos )
       @features = []
