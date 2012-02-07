541cd7dbd3136902a7fe4921178d0770f6fce4e6
Dave Myers <contact@ameravant.com>
2012-02-06 19:40:42 -0800

    fixing error when column_id doesnt have associated record

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -175,7 +175,7 @@ class Admin::PagesController < AdminController
   def find_page
     begin
       @page = Page.find_by_permalink!(params[:id])
-      if @page.main_column_id.blank or Column.find(@page.main_column_id).blank?
+      if @page.main_column_id.blank? or Column.find(@page.main_column_id).blank?
         @page.main_column_id = Column.find_by_title("Default").id
       end
       @menu = @page.menus.first
