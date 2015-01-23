2f3af3474bed40ddfa643cbcb58753a6c0cf41d3
Dave Myers <contact@ameravant.com>
2015-01-23 12:31:39 -0500

    removing cache condition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,7 @@ class PagesController < ApplicationController
   before_filter :authenticate, :only => :show
   unloadable # http://dev.rubyonrails.org/ticket/6001  
   def show
-    if @cms['site_settings']['expires_in'] and @cms['site_settings']['expires_in'] == "0"
-      # Do Nothing
-    elsif @cms['site_settings']['expires_in']
-      expires_in @cms['site_settings']['expires_in'].to_i.minutes, :public => true
-    else
-      expires_in 60.minutes, :public => true
-    end
+    expires_in 60.minutes, :public => true
     get_page_defaults(@page)
   end
   
